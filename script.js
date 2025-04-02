9edb2d5cc48450add20b48fe4d298128eaa55787
vishnuviratGit <vishnuvirat564@gmail.com>
2025-04-02 08:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
+
 const btn = document.getElementById("download-images-button");
 
 const images = [
@@ -7,3 +7,34 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+let loading=document.getElementById("loading");
+let output=document.getElementById("output");
+let errorDiv=document.getElementById("error");
+function downloadImage(url){
+	return new Promise((resolve, reject)=>{
+		  const img=new Image();
+		  img.src=url;
+		  img.onload=()=>resolve(img);
+		  img.onerror=()=>reject(`Failed to download the image ${url}`);
+	})
+}
+
+async function downloadImages() {
+	loading.style.display="block";
+	output.innerHTML="";
+	errorDiv.innerHTML="";
+	
+	try {
+		loading.style.display="none";
+	    const arrPromises= await Promise.all([downloadImage(images[0].url), downloadImage(images[1].url), downloadImage(images[2].url)]);
+		output.style.display="block";
+		arrPromises.forEach((img)=>{
+			output.appendChild(img);
+		})
+	} catch (error) {
+		loading.style.display="none";
+		errorDiv.style.display="block";
+		errorDiv.innerHTML=error;
+	}
+}
+btn.addEventListener("click", downloadImages);
